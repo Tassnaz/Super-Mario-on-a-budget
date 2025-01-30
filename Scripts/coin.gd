e43a59241bc3398a421539b9e972855ecd4e66f7
Tassnaz <tassnass2018@gmail.com>
2025-01-30 13:01:40 +0100

    Update coin.gd

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -8,6 +8,7 @@ func _on_body_entered(body):
 		
 		Global.TotalCoins += 1
 		visible = false
+		$CollisionShape2D.queue_free()
 		$AudioStreamPlayer2D.play(0.1)
 		await get_tree().create_timer(1).timeout
 		queue_free()
